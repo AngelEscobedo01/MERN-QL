fc9af9e20d4e0ff6bb6bd4be1d85fa7318ef62ed
AngelEscobedo1 <35699321+AngelEscobedo1@users.noreply.github.com>
2022-09-04 21:11:18 -0500

    1.0 pushes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
-// import logo from "./logo.svg";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Routes,
+} from "react-router-dom";
 import Header from "./components/Header";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
-import Clients from "./components/Clients";
-import Projects from "./components/Projects";
-import AddClientModal from "./components/AddClientModal";
+import Home from "./pages/Home";
+import Project from "./pages/Project";
+import NotFound from "./pages/NotFound";
 
 const cache = new InMemoryCache({
   typePolicies: {
@@ -34,12 +39,16 @@ function App() {
   return (
     <>
       <ApolloProvider client={client}>
-        <Header />
-        <div className="container">
-          <AddClientModal />
-          <Projects />
-          <Clients />
-        </div>
+        <Router>
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/projects/:id" element={<Project />}></Route>
+              <Route path="*" element={<NotFound />}></Route>
+            </Routes>
+          </div>
+        </Router>
       </ApolloProvider>
     </>
   );
